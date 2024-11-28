0549acf6a3ea1544e1997730443d594a25ce78ba
Nhan Vo <niick7@gmail.com>
2024-11-28 15:01:24 -0600

    Adding products to cart

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Product } from 'src/app/models/product';
 import { ProductService } from '../product.service';
+import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,11 +12,27 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private service: ProductService) {}
+  constructor(
+    private service: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.service.getProducts().subscribe((data) => {
       this.products = data;
+    });
+  }
+
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackBar.open('Product added to cart!', '', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        });
+      },
     });
   }
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { ProductListComponent } from './product-list/product-list.component';
 import { FlexModule } from '@angular/flex-layout';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [ProductListComponent],
-  imports: [CommonModule, MatCardModule, FlexModule],
+  imports: [CommonModule, MatCardModule, FlexModule, MatSnackBarModule],
 })
 export class ProductModule {}
