270bd34fce524051a7308e04b33df76d8856f5e6
Nhan Vo <niick7@gmail.com>
2024-11-28 14:36:51 -0600

    Adding total price

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -9,11 +9,22 @@ import { CartService } from '../cart.service';
 })
 export class CartViewComponent implements OnInit {
   items: Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private service: CartService) {}
   ngOnInit(): void {
     this.service.getCartItems().subscribe((data) => {
       this.items = data;
+      this.totalPrice = this.getTotalPrice();
     });
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    for (let item of this.items) {
+      total += item.price;
+    }
+
+    return total;
   }
 }
